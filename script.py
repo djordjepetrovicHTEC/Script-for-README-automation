cba831cd2987e2106cfc36d5fc8d29506f4c0f0a
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2023-05-08 16:02:18 +0200

    Test v1

--- script.py
+++ script.py
@@ -1,15 +1,1 @@
-import requests
-
-url = "https://github.com/djordjepetrovicHTEC/Script-for-README-automation"
-
-headers = {
-    "Authorization": "Bearer test",
-    "Accept": "application/vnd.github.v3+json"
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    # process the response data
-else:
-    # handle error cases
+print("This is a message from the Python script.")

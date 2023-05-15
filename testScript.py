b5fff590a088f416885d98c04d81546c7e90d04c
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2023-05-15 12:23:24 +0200

    Updating path

--- testScript.py
+++ testScript.py
@@ -1,7 +1,7 @@
 import os
 
 # Specify the path to the file relative to the runner's environment
-file_path = os.path.join(os.environ["GITHUB_WORKSPACE"], "README.md")
+file_path = os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "README.md")
 
 # Open the file in append mode
 with open(file_path, "a") as file:

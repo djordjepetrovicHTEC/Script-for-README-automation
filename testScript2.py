c0fc2f7f4728df6b9f1ae6661878e015df24a489
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2023-05-15 14:44:08 +0200

    test

--- testScript2.py
+++ testScript2.py
@@ -16,11 +16,16 @@ with open(workflow_file_path, 'r') as workflow_file:
 
 # Read the previous version of the testWorkflow1.yaml file from the git history
 prev_version_command = f"git diff {workflow_file_path}"
+
+print(prev_version_command)
+
 prev_version_output = os.popen(prev_version_command).read().splitlines()
 
 # Find the newly added lines in the testWorkflow1.yaml file
 new_lines = difflib.unified_diff(prev_version_output, workflow_contents, lineterm='', n=0)
 new_lines = [line for line in new_lines if line.startswith('+')]
+
+print(new_lines)
 
 # Append the newly added lines to the README.md file
 with open(readme_file_path, 'a') as readme_file:

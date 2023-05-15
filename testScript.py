f07a2d8c624bc48c03f71a4d2bfe2d9a5c268278
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2023-05-15 12:20:01 +0200

    changing path

--- testScript.py
+++ testScript.py
@@ -1,10 +1,7 @@
 import os
 
-# Get the current directory
-current_directory = os.getcwd()
-
-# Specify the path to the file relative to the current directory
-file_path = os.path.join(current_directory, ".github", "workflows", "README.md")
+# Specify the path to the file relative to the runner's environment
+file_path = os.path.join(os.environ["GITHUB_WORKSPACE"], "README.md")
 
 # Open the file in append mode
 with open(file_path, "a") as file:

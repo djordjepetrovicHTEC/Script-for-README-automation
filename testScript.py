2a43385cbb89c847503eb124ab48c812b704b53c
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2023-05-15 12:13:08 +0200

    Checking path and chaning which readme

--- testScript.py
+++ testScript.py
@@ -1,2 +1,12 @@
-with open('README.md', 'a') as readme_file:
-    readme_file.write('test\n')
+import os
+
+# Get the current directory
+current_directory = os.getcwd()
+
+# Specify the path to the file relative to the current directory
+file_path = os.path.join(current_directory, ".github", "workflows", "README.md")
+
+# Open the file in append mode
+with open(file_path, "a") as file:
+    # Append the desired text
+    file.write("test\n")
